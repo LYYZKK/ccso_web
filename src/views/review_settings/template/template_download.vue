f830a2eb7f1a9a3a11a921b96d66210c0e15e927
LYY <liyy@wise2c.com>
2019-05-14 18:01:16 +0800

    资料模板下载页面

---
+++ src/views/review_settings/template/template_download.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="background-color: #ececec; padding: 20px;">
+    <a-row :gutter="16" class="card">
+      <a-col v-for="(item,index) in template" :key="index" >
+        <a-card :title=item :bordered="false">
+          <a-button type="primary">点击下载</a-button>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      template:['绩效数据模板','评审申请模板','现场访谈模板','现场确认模板','现场试算模板']
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .card{
+    display:flex;
+    justify-content: flex-start;
+    align-content: center;
+    flex-wrap: wrap;
+
+    &>div{
+      min-width: 200px;
+      margin: 20px;
+    }
+  }
+</style>
+
+
