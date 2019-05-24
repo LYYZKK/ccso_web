680c8f195d675020a4d22e6aa8b4795dc0c33592
shupeipei <cn.shperry@gmail.com>
2019-05-24 11:05:58 +0800

    optimize jump router logic.

--- src/permission.js
+++ src/permission.js
@@ -9,6 +9,7 @@ import { generateIndexRouter } from "@/utils/util"
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
+// 免登陆白名单路由.
 const whiteList = [
   '/',
   '/website',
@@ -36,52 +37,53 @@ router.beforeEach((to, from, next) => {
     } else {
       if (store.getters.permissionList.length === 0) {
         store.dispatch('GetPermissionList').then(res => {
-              const menuData = res.result;
-              console.log(res.message)
-              if (menuData === null || menuData === "" || menuData === undefined) {
-                return;
-              }
-              let constRoutes = [];
-              constRoutes = generateIndexRouter(menuData);
-              // 添加主界面路由
-              store.dispatch('UpdateAppRouter',  { constRoutes }).then(() => {
-                // 根据roles权限生成可访问的路由表
-                // 动态添加可访问路由表
-                router.addRoutes(store.getters.addRouters)
-                const redirect = decodeURIComponent(from.query.redirect || to.path)
-                if (to.path === redirect) {
-                  // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
-                  next({ ...to, replace: true })
-                } else {
-                  // 跳转到目的路由
-                  next({ path: redirect })
-                }
-              })
-            })
-          .catch(() => {
-            notification.error({
-              message: '系统提示',
-              description: '请求用户信息失败，请重试！'
-            })
-            store.dispatch('Logout').then(() => {
-              next({ path: '/user/login', query: { redirect: to.fullPath } })
-            })
+          const menuData = res.result;
+          console.log(res.message)
+          if (menuData === null || menuData === "" || menuData === undefined) {
+            return;
+          }
+          let constRoutes = [];
+          constRoutes = generateIndexRouter(menuData);
+          // 添加主界面路由
+          store.dispatch('UpdateAppRouter',  { constRoutes }).then(() => {
+            // 根据roles权限生成可访问的路由表
+            // 动态添加可访问路由表
+            router.addRoutes(store.getters.addRouters)
+            const redirect = decodeURIComponent(from.query.redirect || to.path)
+            if (to.path === redirect) {
+              // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+              next({ ...to, replace: true })
+            } else {
+              // 跳转到目的路由
+              next({ path: redirect })
+            }
           })
+        })
+        .catch(() => {
+          notification.error({
+            message: '系统提示',
+            description: '请求用户信息失败，请重试！'
+          })
+          store.dispatch('Logout').then(() => {
+            next({ path: '/user/login', query: { redirect: to.fullPath } })
+          })
+        })
       } else {
         next()
       }
     }
-  } else {
-    if (to.path === '/') {
-      next({ path: '/website'})
-    }
-    if (whiteList.indexOf(to.path) !== -1) {
-      // 在免登录白名单，直接进入
-      next()
-    } else {
+  } else { // 没有 token.
+    if (!whiteList.includes(from.path)) { // 从非白名单跳转而来又没有 token, 原因是 token 失败, 直接进入登录页.
       next({ path: '/user/login', query: { redirect: to.fullPath } })
       NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
+    } else { // 从白名单菜单跳转而来.
+      if (to.path === '/') { // 从白名单跳转到首页.
+        next({ path: '/website'})
+      } else if (whiteList.indexOf(to.path) !== -1) { // 从白名单跳转到白名单, 直接进入.
+        next()
+      }
     }
+
   }
 })
 
