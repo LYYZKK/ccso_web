57f37de51f35a3b497156cac4a27ed1a2985d0f1
lizhigang <lizg@wise2c.com>
2019-06-14 18:38:56 +0800

    提交归档操作开发完成。修复bug

--- src/views/review_project/reviewing/modules/SubmitArchive.vue
+++ src/views/review_project/reviewing/modules/SubmitArchive.vue
@@ -1,7 +1,7 @@
 <template>
   <a-modal
     :title="title"
-    :width="800"
+    :width="1000"
     :visible="visible"
     :confirmLoading="confirmLoading"
     @ok="handleOk"
@@ -9,19 +9,31 @@
     :destroyOnClose="true"
     cancelText="关闭">
     <a-spin :spinning="confirmLoading">
-      <h3>电子证书</h3>
+      <h3 class="devide-title">评审统计</h3>
+      <a-table
+        ref="table"
+        size="middle"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination=false
+        :loading="loading">
+      </a-table>
+      <br/>
+      <h3 class="devide-title">电子证书</h3>
       <a-form :form="form">
         <a-form-item
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="电子证书编号">
-          <a-input placeholder="请输入电子证书编号" v-decorator="['sendBackMsg', {required: true, message: '请输入回退信息！'}]"/>
+          <a-input placeholder="请输入电子证书编号" v-decorator="['certificateNo', {}]"/>
         </a-form-item>
         <a-form-item
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="电子证书日期">
-          <a-date-picker v-decorator="[ 'sceneBeginTime', {}]" format="YYYY-MM-DD"/>
+          <a-date-picker v-decorator="[ 'certificateDate', {}]" format="YYYY-MM-DD"/>
         </a-form-item>
         <a-form-item
           :labelCol="labelCol"
@@ -32,7 +44,7 @@
             :action="FILE_UPLOAD_ACTION"
             :headers="FILE_UPLOAD_HEADERS"
             :data="{'isup':1}"
-            v-decorator="['businessLicenseFile']"
+            v-decorator="['certificateFile', {}]"
             @change="handleChange_2">
             <a-button>
               <a-icon type="upload"/>
@@ -48,9 +60,9 @@
 <script>
   import antMixin from '@/mixins/ant-mixin'
   import constantCfgMixin from '@/mixins/constant.cfg'
-  import {httpAction} from '@/api/manage'
   import AFormItem from "ant-design-vue/es/form/FormItem";
   import ATextarea from "ant-design-vue/es/input/TextArea";
+  import {getAction,httpAction} from '@/api/manage'
 
   export default {
     name: 'SubmitArchive',
@@ -72,15 +84,92 @@
         },
         confirmLoading: false,
         url: {
-          edit: '/review/sendBack/add',
+          submitArchiveUrl: '/review/project/submitArchive',
+          getRecordStatistics: '/review/entryRecord/getRecordStatistics',
         },
         form: this.$form.createForm(this),
         reviewProjectId: '',
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key: 'rowIndex',
+            width: 60,
+            align: 'center',
+            customRender: (t, r, index) => {
+              return parseInt(index) + 1
+            }
+          },
+          {
+            title: '评审类别名称',
+            align: 'center',
+            dataIndex: 'categoryName'
+          },
+          {
+            title: '评判条目总量',
+            align: 'center',
+            dataIndex: 'enrtyNum'
+          },
+          {
+            title: '符合数量',
+            align: 'center',
+            dataIndex: 'correctEnrtyNum'
+          },
+          {
+            title: '不符合数量',
+            align: 'center',
+            dataIndex: 'noCorrectEnrtyNum'
+          },
+          {
+            title: '不适合数量',
+            align: 'center',
+            dataIndex: 'inconformityEnrtyNum'
+          },
+          {
+            title: '达标占比',
+            align: 'center',
+            dataIndex: 'standardValue'
+          },
+          {
+            title: '符合占比',
+            align: 'center',
+            dataIndex: 'correctProportion'
+          },
+          {
+            title: '是否通过',
+            align: 'center',
+            dataIndex: 'isOk',
+            customRender: text => {
+              if (text == 1) {
+                return '通过'
+              } else {
+                return '不通过'
+              }
+            }
+          },
+        ],
+        dataSource: [],
+        formData: {
+          certificateNo: '',
+          certificateDate: '',
+          certificateFile: null,
+          id: ''
+        },
       }
     },
     methods: {
+      handleChange(info) {
+        this.UPLOAD_CHANGE_HANDLER({info, fieldName: 'certificateFile'})
+      },
       edit(id) {
         this.reviewProjectId = id
+        getAction(this.url.getRecordStatistics, {
+          reviewProjectId: this.reviewProjectId
+        }).then((res) => {
+          if (res.success) {
+            this.dataSource = res.result
+          }
+        });
         this.visible = true
       },
 
@@ -93,16 +182,24 @@
         const that = this
         this.form.validateFields((err, values) => {
           if (!err) {
-            var url = ''
-            var param = {}
-            httpAction(url, param, 'post').then((res) => {
+            this.formData.certificateNo = values.certificateNo ? values.certificateNo : null
+            this.formData.certificateDate = values.certificateDate ? values.certificateDate.format('YYYY-MM-DD HH:mm:ss') : null
+            if (values.certificateFile != null) {
+              if (values.certificateFile.file != null) {
+                this.formData.certificateFile = values.certificateFile.file.response.message
+              } else {
+                this.formData.certificateFile = values.certificateFile
+              }
+            }
+            this.formData.id = this.reviewProjectId
+            httpAction(this.url.submitArchiveUrl, this.formData, 'post').then((res) => {
               if (res.success) {
                 that.$message.success(res.message)
                 that.$emit('ok')
               } else {
                 that.$message.warning(res.message)
               }
-              this.form = this.$form.createForm(this)
+              // this.form = this.$form.createForm(this)
             }).finally(() => {
               that.confirmLoading = false
               that.close()
@@ -120,4 +217,7 @@
 </script>
 
 <style scoped>
+  .devide-title {
+    margin-bottom: 20px;
+  }
 </style>
